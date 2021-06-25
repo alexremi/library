4edf17b55018e122d6009211b5cc2f629c11810f
alexremi <remigaylo07@mail.ru>
2021-06-25 22:12:05 +1000

    test added

---
+++ tests/Test1Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class Test1Test extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
